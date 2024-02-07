dbcc06ea286ed96c74f6f7c5c756562440734ba0
nofar12 <81480111+nofar12@users.noreply.github.com>
2024-02-07 12:51:16 +0200

    Update models.py

--- finalPro/signUp/models.py
+++ finalPro/signUp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 class User(models.Model):
-    username = models.CharField(max_length=25, unique=True)
+    username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=25)
