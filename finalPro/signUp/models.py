8ce512d4d7bda26e4f64fa4fc64b209a5c0f8f72
nofar <nofar.geller@gmail.com>
2024-02-07 22:36:02 +0200

    1

--- finalPro/signUp/models.py
+++ finalPro/signUp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-class UserProfile(models.Model):
+class UserProfile(models.Model):  #defining the structure user's database
     username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f'Username: {self.username}, Email: {self.email}, Password: {self.password}'
--- finalPro/signUp/views.py
+++ finalPro/signUp/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .forms import UserRegistrationForm
+from .models import UserProfile
 
-def signUp_user(request): #if user sumbit the sign up form
+
+def signUp_user(request): #adding the data to the database of users
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('success')  # Redirect to success page
+            #return redirect('success')  # Redirect to success page
     else:
         form = UserRegistrationForm()
+
+
     return render(request, 'signUp.html', {'form': form})
+
+
