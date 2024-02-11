d118c28082f989819b7493ce927fcc8063386f62
nofar <nofar.geller@gmail.com>
2024-02-11 23:25:58 +0200

    1

--- finalPro/findRecipe/static/edamamAPI.js
+++ finalPro/findRecipe/static/edamamAPI.js
@@ -2,20 +2,23 @@ const API_KEY = 'dab1922008b44fa6e74f63cb2d195999';
 const API_ENDPOINT = 'https://api.edamam.com/search';
 const APP_ID = 'e862c32a';
 
+function searchRecipes(){
+  const ingredient = document.getElementById('ingredientInput').value;
+  const url =  'https://api.edamam.com/api/recipes/v2?q=' + ingredient + '&type=public&app_id=' + APP_ID + '&app_key=' + API_KEY;
+  var table = document.getElementById('recipeTbl');
+    table.innerHTML = "";
+  var tr = document.createElement('tr');
+  tr.innerHTML = '<tr><th>Name</th><th>Image</th></tr>';
+  table.appendChild(tr);
 
-const searchRecipes = async (query) => {
-  const url = `${API_ENDPOINT}?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`;
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Failed to fetch recipes');
-    }
-    const data = await response.json();
-    return data.hits.map(hit => hit.recipe);
-  } catch (error) {
-    console.error('Error fetching recipes:', error.message);
-    return [];
-  }
-};
-
-
+  fetch(url) //sending request to the url
+        .then(response => response.json()) //find the JSON content of the response - the recipes
+        .then(data => { data.hits.forEach( (recipe) => {
+            var tr = document.createElement('tr');
+            tr.innerHTML = '<td>' + recipe.recipe.label + '</td>' +
+            '<td><img src="' + recipe.recipe.image + '"></td>' ;
+             table.appendChild(tr);
+        } )})
+        //searching for errors
+        .catch(error => console.error('Error fetching recipes:', error.message));
+}
