7e63bad9d7dfb0b0e59fdf1d723f4d6f947313f5
nofar12 <81480111+nofar12@users.noreply.github.com>
2024-06-16 21:33:47 +0300

    Update accountSettings.js

--- ServerSide/accountSettings/accountSettings.js
+++ ServerSide/accountSettings/accountSettings.js
@@ -44,12 +44,12 @@ function changePassword(){
 
 
 document.getElementById('change_pass-form').addEventListener("submit", function(event) {
-    event.preventDefault();  // Prevent default form submission
+    event.preventDefault()  // Prevent default form submission
 
     // Get form data
-    let formData = new FormData(document.getElementById('change_pass-form')); //the formData object  with the form's current keys/values, and encode file input content
-    let password = formData.get('password');
-    let confirm_password = formData.get('confirm-password');
+    let formData = new FormData(document.getElementById('change_pass-form')) // formData object  with the form's current keys/values, and encode file input content
+    let password = formData.get('password')
+    let confirm_password = formData.get('confirm-password')
 
     // Send form data to the server using Fetch API
     fetch('/change_password', {
@@ -59,11 +59,11 @@ document.getElementById('change_pass-form').addEventListener("submit", function(
 
     .then(response => {
         if (response.status === 200) {
-            $("div.success").fadeIn( 300 ).delay( 1500 ).fadeOut( 400 );
-         } else if (response.status === 409) { // username or email already taken
-             $("alert-box failure").fadeIn( 300 ).delay( 1500 ).fadeOut( 400 );
+            $("div.success").fadeIn( 300 ).delay( 1500 ).fadeOut( 400 )
+         } else if (response.status === 409) { 
+             $("alert-box failure").fadeIn( 300 ).delay( 1500 ).fadeOut( 400 )
         } else {
-            $("alert-box failure").fadeIn( 300 ).delay( 1500 ).fadeOut( 400 );
+            $("alert-box failure").fadeIn( 300 ).delay( 1500 ).fadeOut( 400 )
         }
     })
 })
